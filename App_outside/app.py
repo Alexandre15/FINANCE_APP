fb2b684e30829824de92d2df963fe4a3de7231b3
Alexandre Fagundes <alefagundes155@gmail.com>
2025-08-07 23:43:01 -0300

    AppOut

--- App_outside/app.py
+++ App_outside/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
 # from sqlalchemy import create_engine, Column, Integer, String
 
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home.html")
+
+
+@app.route('/dados')
+def dados():
+    df = pd.read_excel('dados.xlsx')
+    dados_json = df.to_dict(orient='records')
+    return jsonify(dados_json)
 
 
 if __name__ == "__main__":
