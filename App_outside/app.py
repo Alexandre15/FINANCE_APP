22f3c7e916ae7c89a03969185a159147a343b0bc
Alexandre Fagundes <alefagundes155@gmail.com>
2025-08-07 23:44:44 -0300

    AppOut

--- App_outside/app.py
+++ App_outside/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
 import pandas as pd
 # from sqlalchemy import create_engine, Column, Integer, String
 
